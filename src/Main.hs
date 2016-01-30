ec4b0b625fe18c97860958aecc697a3d042ea8bb
Alex Belanger <i.caught.air@gmail.com>
2016-01-30 18:03:30 -0500

    WIP haskell

--- src/Main.hs
+++ src/Main.hs
@@ -1,1 +1,37 @@
-main = putStrLn "Hello World!"
+{-# LANGUAGE OverloadedStrings #-}
+
+import SDL (($=))
+import SDL.Video
+import SDL.Video.Renderer
+import SDL.Event
+import Control.Monad
+import Linear.V4 (V4(V4))
+import Data.Word
+
+main = do
+    -- x <- getDisplays
+    -- print x    
+    -- idealWindowSize <- displayModeSize . head . displayModes . head <$> getDisplays TODO: doesnt work without initialized?
+    window <- createWindow "LoneSome Space" defaultWindow { windowMode = FullscreenDesktop }
+    renderer <- createRenderer window (-1) defaultRenderer
+    
+    showWindow window
+    disableScreenSaver
+
+    rendererDrawColor renderer $= V4 0 0 0 0 -- black
+    mainLoop renderer
+
+    destroyRenderer renderer
+    destroyWindow window
+
+mainLoop renderer = do
+    clear renderer
+    present renderer
+
+    event <- waitEvent
+    
+    let quit = eventPayload event == QuitEvent
+
+    -- putStrLn "Event!"
+    
+    unless quit $ mainLoop renderer
