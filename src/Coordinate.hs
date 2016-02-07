36065b18f6c704a9b5453bcb76d166e37a88ae06
Alex Belanger <i.caught.air@gmail.com>
2016-02-07 00:39:45 -0500

    Fixed few errors

--- src/Coordinate.hs
+++ src/Coordinate.hs
@@ -1,6 +1,6 @@
 module Coordinate where
 
-import Linear (V2(V2), V4(V4))
+import Linear (V2(V2))
 import Linear.Affine (Point(P))
 import Control.Lens
 
@@ -13,5 +13,6 @@ coordinateX f s = let (P (V2 cx cy)) = getCoordinate s in
 coordinateY :: Lens' Coordinate Integer
 coordinateY f s = let (P (V2 cx cy)) = getCoordinate s in
     (\y -> MkCoordinate $ P $ V2 cx y) <$> f cy
-    
+
+defaultCoordinate :: Coordinate
 defaultCoordinate = MkCoordinate $ P $ V2 0 0
---
+++ src/Renderer.hs
@@ -0,0 +1,2 @@
+module Renderer where
+
