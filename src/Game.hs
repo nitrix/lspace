04d15b70dca59e0bd1e72841ab946482f3c5513d
Alex Belanger <i.caught.air@gmail.com>
2016-02-14 21:07:47 -0500

    Pretty big refactor

--- src/Game.hs
+++ src/Game.hs
@@ -2,12 +2,26 @@ module Game where
 
 import Camera
 import Control.Lens
+import Control.Monad.Reader
 import Control.Monad.State
 import Coordinate
-import SDL.Event
-import SDL.Input.Keyboard
+import Linear (V2(V2))
+import Linear.Affine (Point(P))
+import Object
+import SDL
 import World
 
+-- | Convenience type to express computations that needs the Environment' to do their work
+type Environment m a = ReaderT Environment' m a
+
+-- | Contains the assets needed to run the game (things that will not change over time)
+data Environment' = MkEnvironment
+    { envWindow   :: Window
+    , envRenderer :: Renderer
+    , envTileset  :: Texture
+    }
+
+-- | Contains the state of the game (things that will change over time)
 data Game = MkGame
     { _playerPosition :: Coordinate
     , _camera         :: Camera
@@ -22,6 +36,7 @@ camera f s = (\x -> s { _camera = x }) <$> f (_camera s)
 world :: Lens' Game World
 world f s = (\x -> s { _world = x }) <$> f (_world s)
 
+-- | Default game state with an empty world, player and camera at 0,0
 defaultGame :: Game
 defaultGame = MkGame
     { _playerPosition = defaultCoordinate
@@ -57,3 +72,42 @@ gameHandleKeyboardEvent ked =
         keysym    = keyboardEventKeysym ked    -- ^ Key symbol information with two representations available: keycode or scancode
         keycode   = keysymKeycode keysym       -- ^ Which character is received from the operating system
         scancode  = keysymScancode keysym      -- ^ Physical key location as it would be on a US QWERTY keyboard
+
+-- --------------------------------------------------------------------------------------------------------
+-- TODO: to refactor everything below this line
+-- --------------------------------------------------------------------------------------------------------
+renderGame :: Game -> Environment IO ()
+renderGame game = do
+    renderer <- asks envRenderer
+    window   <- asks envWindow
+    tileset  <- asks envTileset
+
+    -- Let's prepare a new fresh screen
+    clear renderer
+
+    V2 width height <- SDL.get $ windowSize window
+    
+    let screenWidthInTiles = fromIntegral $ width `div` 32
+    let screenHeightInTiles = fromIntegral $ height `div` 32
+    
+    let cameraX = game ^. camera . cameraCoordinate . coordinateX
+    let cameraY = game ^. camera . cameraCoordinate . coordinateY
+    
+    let coordsToRender = [coordinate x y
+                         | x <- [cameraX-1..cameraX+screenWidthInTiles+1]
+                         , y <- [cameraY-1..cameraY+screenHeightInTiles+1]
+                         ]
+
+    let objectsToRender = concatMap (\x -> (\y -> (x, y)) <$> worldObjectsAt (game ^. world) x) coordsToRender
+    
+    mapM_ (\(coord, obj) -> do
+        let tileRelX = fromIntegral $ coord ^. coordinateX - cameraX
+        let tileRelY = fromIntegral $ coord ^. coordinateY - cameraY
+        let (objSpriteX, objSpriteY) = objectSprite obj
+        let src = Rectangle (P $ (V2 (fromIntegral objSpriteX) (fromIntegral objSpriteY)) * V2 32 32) (V2 32 32)
+        let dst = Rectangle (P $ V2 (tileRelX*32) (tileRelY*32)) (V2 32 32)
+        copyEx renderer tileset (Just src) (Just dst) 0 Nothing (V2 False False)
+        ) objectsToRender
+
+    -- Render new screen
+    present renderer 
--- src/Main.hs
+++ src/Main.hs
@@ -1,42 +1,26 @@
 {-# LANGUAGE OverloadedStrings #-}
 
 import SDL
-import qualified SDL.Image as IM
+import qualified SDL.Image as Img
 import Control.Monad
-import Linear (V2(V2), V4(V4))
-import Linear.Affine (Point(P))
+import Linear (V4(V4))
 import Control.Monad.Reader
 import Control.Monad.State
-import Control.Lens
 
-import Camera
-import Coordinate
 import Game
-import World
-import Object
-
-type Environment m a = ReaderT Environment' m a
-
-data Environment' = MkEnvironment
-    { envGame     :: Game
-    , envWindow   :: Window
-    , envRenderer :: Renderer
-    , envTileset  :: Texture
-    }
-
 
 main :: IO ()
 main = do
     -- Initialize SDL an SDL_image
     initializeAll
-    IM.initialize [IM.InitPNG]
+    Img.initialize [Img.InitPNG]
 
     -- Fullscreen window with the default renderer
     window <- createWindow "LoneSome Space" defaultWindow { windowMode = FullscreenDesktop }
     renderer <- createRenderer window (-1) defaultRenderer
     
     -- Loading texture
-    tileset <- IM.loadTexture renderer "assets/tileset.png"
+    tileset <- Img.loadTexture renderer "assets/tileset.png"
     
     -- Some options for convenience
     disableScreenSaver
@@ -48,9 +32,8 @@ main = do
     showWindow window
 
     -- Main loop
-    runReaderT mainLoop $ MkEnvironment
-        { envGame     = defaultGame
-        , envWindow   = window
+    runReaderT (mainLoop defaultGame) $ MkEnvironment
+        { envWindow   = window
         , envRenderer = renderer
         , envTileset  = tileset
         }
@@ -58,57 +41,19 @@ main = do
     -- Cleanup
     destroyRenderer renderer
     destroyWindow window
-    IM.quit
-    SDL.quit
+    Img.quit
+    quit
 
-mainLoop :: Environment IO ()
-mainLoop = do
+mainLoop :: Game -> Environment IO ()
+mainLoop game = do
     -- Wait for any event
     event <- waitEvent
     
     -- Handle game event
-    game <- asks envGame
     let (halt, newGame) = runState (gameHandleEvent event) game
     
-    -- Render world
-    renderGame
+    -- Render the game
+    renderGame newGame
 
     -- Do it again
-    unless halt $ withReaderT (\x -> x { envGame = newGame}) mainLoop
-
-renderGame :: Environment IO ()
-renderGame = do
-    game     <- asks envGame
-    renderer <- asks envRenderer
-    window   <- asks envWindow
-    tileset  <- asks envTileset
-
-    -- Let's prepare a new fresh screen
-    clear renderer
-
-    V2 width height <- SDL.get $ windowSize window
-    
-    let screenWidthInTiles = fromIntegral $ width `div` 32
-    let screenHeightInTiles = fromIntegral $ height `div` 32
-    
-    let cameraX = game ^. camera . cameraCoordinate . coordinateX
-    let cameraY = game ^. camera . cameraCoordinate . coordinateY
-    
-    let coordsToRender = [coordinate x y
-                         | x <- [cameraX-1..cameraX+screenWidthInTiles+1]
-                         , y <- [cameraY-1..cameraY+screenHeightInTiles+1]
-                         ]
-
-    let objectsToRender = concatMap (\x -> (\y -> (x, y)) <$> worldObjectsAt (game ^. world) x) coordsToRender
-    
-    mapM_ (\(coord, obj) -> do
-        let tileRelX = fromIntegral $ coord ^. coordinateX - cameraX
-        let tileRelY = fromIntegral $ coord ^. coordinateY - cameraY
-        let (objSpriteX, objSpriteY) = objectSprite obj
-        let src = Rectangle (P $ (V2 (fromIntegral objSpriteX) (fromIntegral objSpriteY)) * V2 32 32) (V2 32 32)
-        let dst = Rectangle (P $ V2 (tileRelX*32) (tileRelY*32)) (V2 32 32)
-        copyEx renderer tileset (Just src) (Just dst) 0 Nothing (V2 False False)
-        ) objectsToRender
-
-    -- Render new screen
-    present renderer 
+    unless halt $ mainLoop newGame
