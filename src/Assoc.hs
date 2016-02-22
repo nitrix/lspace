c220dc5e066c803cbc31f9c7efb7c75a8074a677
Alex Belanger <i.caught.air@gmail.com>
2016-02-22 14:12:44 -0500

    Small wins!

--- src/Assoc.hs
+++ src/Assoc.hs
@@ -9,6 +9,7 @@ where
 import Data.Maybe
 import qualified Data.Map as M
 import qualified Data.Set as S
+import Data.Tuple
 
 data Assoc a b = MkAssoc (M.Map a (S.Set b)) (M.Map b (S.Set a))
 
@@ -21,7 +22,6 @@ lookupR k (MkAssoc _ right) = fromMaybe S.empty $ M.lookup k right
 fromList :: (Ord a, Ord b) => [(a, b)] -> Assoc a b
 fromList xs = MkAssoc
     (foldr innerInsert M.empty xs)
-    (foldr innerInsert M.empty $ innerSwap <$> xs)
+    (foldr innerInsert M.empty $ swap <$> xs)
     where
         innerInsert (a, b) m = M.insertWith S.union a (S.singleton b) m
-        innerSwap (a, b) = (b, a) -- TODO: swap from Data.Tuple ?
