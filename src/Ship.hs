6b32c349315ae6ee5f372f42f2cd9e116dc666de
Alex Belanger <i.caught.air@gmail.com>
2016-04-07 14:27:01 -0400

    Small progress

--- src/Ship.hs
+++ src/Ship.hs
@@ -4,19 +4,24 @@ import Coordinate
 import qualified Data.Map as M
 import qualified Data.Vector as V
 import Object
+import SDL
+import Linear (V2(..))
+import Linear.Affine (Point(..))
 
 data Ship = MkShip
     { shipMass       :: Integer
     -- , shipVelocity   :: Integer
     -- , shipDirection  :: Direction
     -- , shipCoordinate :: Coordinate
-    , shipChunks     :: M.Map Coordinate (V.Vector Object)
+    , shipChunks     :: M.Map Coordinate (Texture, V.Vector Object)
     }
 
-{-
-insert :: Bool
-insert = undefined
+insert :: Coordinate -> (Texture, V.Vector Object) -> Ship -> Ship
+insert c p = undefined
+    where
+        nc = let (P (V2 x y)) = getCoordinate c in coordinate (x `div` 10) (y `div` 10)
 
+{-
 adjust :: Bool
 adjust = undefined
 
