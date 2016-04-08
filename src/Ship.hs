3569f328d58eb8c918a4ee22d0cdd0caa5e8ce27
Alex Belanger <i.caught.air@gmail.com>
2016-04-08 14:31:07 -0400

    Small progress, Ship module

--- src/Ship.hs
+++ src/Ship.hs
@@ -1,32 +1,47 @@
 module Ship where
 
 import Coordinate
+import Control.Lens
+import Data.Maybe
 import qualified Data.Map as M
+import qualified Data.Set as S
 import qualified Data.Vector as V
 import Object
-import SDL
 import Linear (V2(..))
 import Linear.Affine (Point(..))
 
 data Ship = MkShip
-    { shipMass       :: Integer
-    -- , shipVelocity   :: Integer
-    -- , shipDirection  :: Direction
-    -- , shipCoordinate :: Coordinate
-    , shipChunks     :: M.Map Coordinate (Texture, V.Vector Object)
+    { _shipMass         :: Integer
+    , _shipVelocity     :: Integer
+    , _shipDirection    :: Direction
+    , _shipCoordinate   :: Coordinate
+    , _shipRecentChunks :: S.Set Coordinate -- ^ List of recently mutated chunks
+    , _shipChunks       :: M.Map Coordinate (V.Vector [Object])
     }
 
-insert :: Coordinate -> (Texture, V.Vector Object) -> Ship -> Ship
-insert c p = undefined
+chunkCoord c = let (x, y) = tupleCoord c in coordinate (x `div` 10) (y `div` 10)
+tupleCoord c = let (P (V2 x y)) = getCoordinate c in (x, y)
+chunkIdx c = let (x, y) = tupleCoord c in fromInteger $ (y `mod` 10) * 10 + (x `mod` 10)
+        
+insert :: Coordinate -> Object -> Ship -> Ship
+insert coord object ship = ship
+    { _shipChunks = M.insertWith mutChunk (chunkCoord coord) newChunk (_shipChunks ship)
+    , _shipMass = (_shipMass ship) + 1
+    , _shipRecentChunks = S.insert (chunkCoord coord) (_shipRecentChunks ship)
+    }
     where
-        nc = let (P (V2 x y)) = getCoordinate c in coordinate (x `div` 10) (y `div` 10)
+        mutChunk new old = old V.// [(chunkIdx coord, object : (old V.! chunkIdx coord))]
+        newChunk = V.replicate 100 [] V.// [(chunkIdx coord, [object])]
+        
+lookupChunk :: Coordinate -> Ship -> Maybe (V.Vector [Object])
+lookupChunk coord ship = M.lookup (chunkCoord coord) (_shipChunks ship)
+
+lookupCell :: Coordinate -> Ship -> [Object]
+lookupCell coord ship = fromMaybe [] $ (V.! chunkIdx coord) <$> lookupChunk coord ship
 
 {-
 adjust :: Bool
 adjust = undefined
-
-lookup :: Bool
-lookup = undefined
 
 range :: Bool
 range = undefined
