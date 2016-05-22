8c3860860db7cb807e718bb58c4a916f6ad65d22
Alex Belanger <i.caught.air@gmail.com>
2016-05-22 01:24:13 -0400

    Viable Grid

--- src/Grid.hs
+++ src/Grid.hs
@@ -8,31 +8,28 @@ import Data.Foldable (foldl')
 import qualified Data.Set as S
 
 class Gridable c k | c -> k where
-    extractX :: Ord k => c -> k
-    extractY :: Ord k => c -> k
+    extractX :: (Eq c, Ord k) => c -> k
+    extractY :: (Eq c, Ord k) => c -> k
 
 data GridAxis = AxisX | AxisY deriving (Eq, Ord, Show)
 
 -- | Grid c v is a spatial partitioning data structure that maps a coordinate type @c to a set of values of type @v.
 data Grid c k v = MkGrid
     { gLeft   :: Grid c k v
-    -- , gPivot :: k
     , gCenter :: (c, S.Set v)
-    -- , gCenter :: Map c (S.Set v)
     , gRight  :: Grid c k v
     , gAxis   :: GridAxis
-    }
-    | MkGridCell c v
-    | MkGridEmpty
+    } | GridEmpty GridAxis
     deriving (Eq, Ord, Show)
 
 empty :: Grid c k v
-empty = MkGridEmpty AxisX
+empty = GridEmpty AxisX
 
-fromList :: (Gridable c k, Ord k, Ord v) => [(c, v)] -> Grid c k v
+fromList :: (Gridable c k, Eq c, Ord k, Ord v) => [(c, v)] -> Grid c k v
 fromList xs = foldl' (\g x -> uncurry insert x g) empty xs
 
-insert :: (Gridable c k, Ord k, Ord v) => c -> v -> Grid c k v -> Grid c k v
+-- Tested
+insert :: (Gridable c k, Eq c, Ord k, Ord v) => c -> v -> Grid c k v -> Grid c k v
 insert coord value (GridEmpty axis) = MkGrid
     { gLeft   = GridEmpty $ oppAxis axis
     , gCenter = (coord, S.singleton value)
@@ -42,20 +39,24 @@ insert coord value (GridEmpty axis) = MkGrid
     where
         oppAxis a = if a == AxisX then AxisY else AxisX
 insert coord value grid = case (separator coord) `compare` (separator pivot) of
-    LT -> grid { gLeft   = insert coord value $ gLeft grid }
-    EQ -> grid { gCenter = S.insert value <$> gCenter grid }
-    GT -> grid { gRight  = insert coord value $ gRight grid }
+    LT -> grid { gLeft = insert coord value $ gLeft grid }
+    EQ -> if coord == pivot
+          then grid { gCenter = S.insert value <$> gCenter grid }
+          else grid { gLeft = insert coord value $ gLeft grid }
+    GT -> grid { gRight = insert coord value $ gRight grid }
     where
         pivot     = fst . gCenter $ grid
         separator = if gAxis grid == AxisX then extractX else extractY
 
 -- Tested
-lookup :: (Gridable c k, Ord k) => c -> Grid c k v -> S.Set v
+lookup :: (Gridable c k, Eq c, Ord k) => c -> Grid c k v -> S.Set v
 lookup _ (GridEmpty _) = S.empty
 lookup coord grid = case (separator coord) `compare` (separator pivot) of
-    LT -> lookup coord  $ gLeft grid
-    EQ -> snd . gCenter $ grid
-    GT -> lookup coord  $ gRight grid
+    LT -> lookup coord $ gLeft grid
+    EQ -> if coord == pivot
+          then snd . gCenter $ grid
+          else lookup coord $ gLeft grid
+    GT -> lookup coord $ gRight grid
     where
         pivot = fst . gCenter $ grid
         separator = if gAxis grid == AxisX then extractX else extractY
@@ -64,4 +65,4 @@ remove :: c -> v -> Grid c k v -> Grid c k v
 remove coord value grid = grid -- TODO
 
 range :: c -> c -> Grid c k v -> S.Set v 
-range low high grid = S.empty -- set unions
+range low high grid = S.empty -- TODO: set unions
