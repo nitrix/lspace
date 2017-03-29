54d4f3b43309d1924c45492574586f02c04cd4e8
nitrix <i.caught.air@gmail.com>
2017-03-29 18:45:04 -0400

    Fiddling with Relational

--- src/Main.hs
+++ src/Main.hs
@@ -6,7 +6,7 @@ import Kawaii
 main :: IO ()
 main = runApp $ App
     { appTitle  = "Lonesome Space"
-    , appMode   = Fullscreen
+    , appMode   = Windowed 800 600 -- Fullscreen
     , appScenes = [menu]
     }
 
@@ -15,15 +15,31 @@ menu = Scene
     { sceneUpdate = const (return Success)
     , sceneRender = return ()
     }
+
+menu = Scene menuUpdate menuRender
+
+menuUpdate :: Event -> Engine Result
+menuUpdate event = do
+    case event of
+        EventKeyPressed key -> if key == KeycodeEscape then return Terminate else return Success
+        _                   -> return Skip
+
+menuRender :: Engine ()
+menuRender = return ()
 -}
 
-import Control.Concurrent
 import Kawaii.Core
--- import Kawaii.Engine -- different engines like SDL or other
-import Relational
+-- import Relational
 
 main :: IO ()
 main = runApp $ App
     { appTitle = "Lonesome Space"
     , appMode  = Fullscreen
     }
+
+{-
+main :: IO ()
+main = runRelational "test" $ do
+    foo <- newRelation (42 :: Int)
+    writeRelation foo 69
+-}
