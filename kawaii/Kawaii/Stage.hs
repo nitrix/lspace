4612c322ea659ee5da274f5780eac5e546176340
nitrix <i.caught.air@gmail.com>
2017-04-19 23:34:00 -0400

    Less rage

--- kawaii/Kawaii/Stage.hs
+++ kawaii/Kawaii/Stage.hs
@@ -6,12 +6,7 @@ module Kawaii.Stage
     ( Stage(Stage)
     , Updating
     , Rendering
-    , bring
-    , destroy
-    , skip
-    , success
-    , switch
-    , terminate
+    , Result(..)
     ) where
 
 import Control.Monad.Reader
@@ -32,41 +27,31 @@ data RenderContext = RenderContext
     }
 
 -- Aliases for convenience
-type Updating c = UpdateM c ()
+type Updating c = UpdateM c Game (Result c)
 type Rendering c = StateT c (ReaderT RenderContext IO) ()
 
--- newtype Updating c = Updating { runUpdating :: StateT (c, [Stage c]) Game () }
-newtype UpdateM s a = UpdateM { unwrapUpdateM :: StateT (s, [Stage s], [Stage s]) Game (Result s) } deriving (Functor, Applicative)
+newtype UpdateM s m a = Updating { runUpdateM :: StateT s m a } deriving (Functor, Applicative, Monad, MonadState s)
+-- newtype UpdateM s a = UpdateM { unwrapUpdateM :: StateT (s, [Stage s], [Stage s]) Game a } deriving (Functor, Applicative)
 
+{-
 instance Monad (UpdateM s) where
     return a = UpdateM $ return a
     Skipped   >>= f = _
     Succeeded >>= f = return Succeeded
+-}
 
+{-
 instance MonadState s (UpdateM s) where
     get = UpdateM $ get >>= return . (\(x,_,_) -> x)
     put x = UpdateM $ modify (\(_, seenStages, futureStages) -> (x, seenStages, futureStages))
+-}
 
-stageModify :: ([Stage s] -> [Stage s]) -> UpdateM s ()
-stageModify f = UpdateM $ state (\(s, seenStages, futureStages) -> ((), (s, f stages)))
+-- stageModify :: ([Stage s] -> [Stage s]) -> UpdateM s ()
+-- stageModify f = UpdateM $ state (\(s, seenStages, futureStages) -> ((), (s, f stages)))
 
-data Result s = Skipped
-              | Succeeded
-
-skip :: UpdateM s (Result s)
-skip = UpdateM $ return Skipped
-
-success :: Updating c
-success = undefined
-
-terminate :: Updating c
-terminate = undefined
-
-bring :: Stage c -> Updating c
-bring _stage = undefined
-
-switch :: Stage c -> Updating c
-switch _stage = undefined
-
-destroy :: Updating c
-destroy = undefined
+data Result c = Success
+              | Skip
+              | Switch (Stage c)
+              | Bring (Stage c)
+              | Destroy
+              | Terminate
