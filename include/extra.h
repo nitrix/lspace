0ab0e05d207f6f19e88439b7091c60ad22d74e4e
nitrix <gc3teu89fawugva4@nitrix.me>
2019-07-08 00:29:39 -0400

    More skeleton in place.

---
+++ include/extra.h
@@ -0,0 +1,6 @@
+#ifndef LSPACE_EXTRA_H
+#define LSPACE_EXTRA_H
+
+#define UNUSED(X) ((void) (X))
+
+#endif
---
+++ src/main.c
@@ -0,0 +1,10 @@
+#include "main.h"
+
+int main(int argc, char *argv[]) {
+    UNUSED(argc);
+    UNUSED(argv);
+
+    printf("Hello World!\n");
+
+    return EXIT_SUCCESS;
+}
